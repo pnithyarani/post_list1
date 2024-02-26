644559cc727ee47bfd28b223cefe537961c43257
pnithyarani <160702503+pnithyarani@users.noreply.github.com>
2024-02-26 12:30:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import AddPostsForm from './features/posts/AddPostsForm';
+import PostsList from './features/posts/postsList';
+
+
+function App() {
+  return (
+    <main>
+      <AddPostsForm/>
+      <PostsList/>
+    </main>
+  );
+}
+
+export default App;
